a52a9d58f77a4286e7b825e05ff517ca52c9a69f
dan <43108050+dumbkiddo@users.noreply.github.com>
2020-12-08 16:17:31 +0600

    Initial commit

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public GameObject bulletToFire;
+    public Transform firePoint;
+
+    public float timeBetweenShots;
+    private float shotCounter;
+
+    public string weaponName;
+    public Sprite gunUI;
+
+    public int itemCost;
+    public Sprite gunShopSprite;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (PlayerController.instance.canMove )
+        {
+            if (shotCounter > 0)
+            {
+                shotCounter -= Time.deltaTime;
+            }
+            else
+            {
+
+                if (Input.GetMouseButtonDown(0) || Input.GetMouseButton(0))
+                {
+                    Instantiate(bulletToFire, firePoint.position, firePoint.rotation);
+                    shotCounter = timeBetweenShots;
+                    
+
+                }
+
+              
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,161 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public static PlayerController instance;
+
+    public float moveSpeed;
+    private Vector2 moveInput;
+
+    public Rigidbody2D theRB;
+
+    public Transform gunArm;
+
+    public Animator anim;
+
+    public SpriteRenderer bodySR;
+
+    private float activeMoveSpeed;
+    public float dashSpeed = 8f, dashLength = .5f, dashCooldown = 1f, dashInvincibility = .5f;
+    [HideInInspector]
+    public float dashCounter;
+    private float dashCoolCounter;
+
+    [HideInInspector]
+    public bool canMove = true;
+
+    public List<Gun> availableGuns = new List<Gun>();
+    [HideInInspector]
+    public int currentGun;
+
+    private void Awake()
+    {
+        instance = this;
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+   
+    void Start()
+    {
+        activeMoveSpeed = moveSpeed;
+
+        UIController.instance.currentGun.sprite = availableGuns[currentGun].gunUI;
+        UIController.instance.gunText.text = availableGuns[currentGun].weaponName;
+    }
+
+  
+    void Update()
+    {
+        if (canMove)
+        {
+            moveInput.x = Input.GetAxisRaw("Horizontal");
+            moveInput.y = Input.GetAxisRaw("Vertical");
+
+            moveInput.Normalize();
+
+            //transform.position += new Vector3(moveInput.x * Time.deltaTime * moveSpeed, moveInput.y * Time.deltaTime * moveSpeed, 0f);
+
+            theRB.velocity = moveInput * activeMoveSpeed;
+
+            Vector3 mousePos = Input.mousePosition;
+            Vector3 screenPoint = CameraController.instance.mainCamera.WorldToScreenPoint(transform.localPosition);
+
+            if (mousePos.x < screenPoint.x)
+            {
+                transform.localScale = new Vector3(-1f, 1f, 1f);
+                gunArm.localScale = new Vector3(-1f, -1f, 1f);
+            }
+            else
+            {
+                transform.localScale = Vector3.one;
+                gunArm.localScale = Vector3.one;
+            }
+
+           
+            Vector2 offset = new Vector2(mousePos.x - screenPoint.x, mousePos.y - screenPoint.y);
+            float angle = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
+            gunArm.rotation = Quaternion.Euler(0, 0, angle);
+
+
+            if(Input.GetKeyDown(KeyCode.Tab))
+            {
+                if(availableGuns.Count > 0)
+                {
+                    currentGun++;
+                    if(currentGun >= availableGuns.Count)
+                    {
+                        currentGun = 0;
+                    }
+
+                    SwitchGun();
+
+                } else
+                {
+                    Debug.LogError("Player has no guns!");
+                }
+            }
+
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                if (dashCoolCounter <= 0 && dashCounter <= 0)
+                {
+                    activeMoveSpeed = dashSpeed;
+                    dashCounter = dashLength;
+
+                    anim.SetTrigger("dash");
+                }
+            }
+
+            if (dashCounter > 0)
+            {
+                dashCounter -= Time.deltaTime;
+                if (dashCounter <= 0)
+                {
+                    activeMoveSpeed = moveSpeed;
+                    dashCoolCounter = dashCooldown;
+                }
+            }
+
+            if (dashCoolCounter > 0)
+            {
+                dashCoolCounter -= Time.deltaTime;
+            }
+
+
+
+
+
+            if (moveInput != Vector2.zero)
+            {
+                anim.SetBool("isMoving", true);
+            }
+            else
+            {
+                anim.SetBool("isMoving", false);
+            }
+        } else
+        {
+            theRB.velocity = Vector2.zero;
+            anim.SetBool("isMoving", false);
+        }
+    }
+
+
+    public void SwitchGun()
+    {
+        foreach(Gun theGun in availableGuns)
+        {
+            theGun.gameObject.SetActive(false);
+        }
+
+        availableGuns[currentGun].gameObject.SetActive(true);
+
+        UIController.instance.currentGun.sprite = availableGuns[currentGun].gunUI;
+        UIController.instance.gunText.text = availableGuns[currentGun].weaponName;
+
+    }
+}
